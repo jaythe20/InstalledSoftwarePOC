fda9081de5c9207fdcac0ba9e1f43518318ecabf
Jay Thakkar <jaythe20@gmail.com>
2018-05-02 10:25:02 +0530

    add list  excluded software.

--- InstalledSoft/Program.cs
+++ InstalledSoft/Program.cs
@@ -9,31 +9,35 @@ namespace InstalledSoft
     {
         static void Main(string[] args)
         {
+            List<string> excludedFolder = new List<string>() { "Common Files", "Internet Explorer", "Windows NT", "Intel", "Bonjour", "Common7", "AppInsights", "Goodix", "Dell" };
+
             Dictionary<string, DateTime> folderNameandTime = new Dictionary<string, DateTime>();
 
             foreach (var files in Directory.GetDirectories(@"C:\Program Files (x86)"))
             {
                 FileInfo info = new FileInfo(files);
-                folderNameandTime.Add(info.FullName, info.CreationTimeUtc);
+                if (!excludedFolder.Contains(info.Name))
+                    folderNameandTime.Add(info.FullName, info.CreationTimeUtc);
             }
 
             foreach (var files in Directory.GetDirectories(@"C:\Program Files"))
             {
                 FileInfo info = new FileInfo(files);
-                folderNameandTime.Add(info.FullName, info.CreationTimeUtc);
+                if (!excludedFolder.Contains(info.Name))
+                    folderNameandTime.Add(info.FullName, info.CreationTimeUtc);
             }
 
             Dictionary<DateTime, List<string>> folderName = folderNameandTime.GroupBy(a => a.Value.Date).ToDictionary(t => t.Key, t => t.Select(a => string.Format("{0}, {1}", Path.GetFileName(a.Key), a.Value)).ToList());
-            
+
             using (StreamWriter file = new StreamWriter(@"C:\InstalledSoftware.txt"))
             {
-                foreach (var entry in folderName.OrderBy(a=>a.Key.Date))
+                foreach (var entry in folderName.OrderBy(a => a.Key.Date))
                 {
                     file.WriteLine();
                     file.WriteLine("[Created Date - {0}]", entry.Key.ToString("MM/dd/yyyy"));
 
                     var folderData = entry.Value.Select(a => a.Split(',').ToList());
-                    var listname = folderData.GroupBy(a => Convert.ToDateTime(a[1]).ToString("HH:mm")).ToDictionary(t => t, t => t.Select(a => string.Format("{0}, {1}", Path.GetFileName(a[0]), a[1])).ToList());
+                    var listname = folderData.GroupBy(a => Convert.ToDateTime(a[1]).ToString("HH:mm:ss")).ToDictionary(t => t, t => t.Select(a => string.Format("{0}, {1}", Path.GetFileName(a[0]), a[1])).ToList());
 
                     foreach (var item in listname.OrderBy(a => a.Key.Key))
                     {
